96a19e2fa07a7166fee0350b40d07f4b123c8b21
GingerApril <126075181+GingerApril@users.noreply.github.com>
2023-05-09 13:35:59 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index() => View();
+        public IActionResult About() => View();
+        public IActionResult Predict() => View();
+        public IActionResult Cart() => View();
+        public IActionResult Login() => View();
+        public IActionResult Profile() => View();
+        public IActionResult Goods() => View();
+        public IActionResult Services() => View();
+    }
+}
